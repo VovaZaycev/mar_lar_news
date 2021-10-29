14ec4efcb53894e2d1988c99e939803d5ed3ce63
VovaZaycev <vovazaycev0306@gmail.com>
2021-10-29 16:33:47 +0300

    view of default layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Articles</title>
+
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+    </nav>
+</div>
+@yield('content')
+</body>
+</html>
